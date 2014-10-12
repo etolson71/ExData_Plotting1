e8f6bef23164c69f5e5c82adca2b128a51019cf3
etolson <etolson71@yahoo.com>
2014-10-11 22:18:38 -0400

    Changed the axis and label scalings
    Needed to change the axis and label font scaling for all graphs using
    the cex.axis and cex.label parameters in the plots.  Makes it look more
    like the ones on the website.

--- plot4.R
+++ plot4.R
@@ -21,13 +21,13 @@ dev.copy(png,'plot4.png')
 par(mfrow=c(2,2))
 
 #Plot for top left
-plot(elect1$datetm,elect1$Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)")
+plot(elect1$datetm,elect1$Global_active_power,type="l",xlab="",ylab="Global Active Power",cex.axis=0.8,cex.lab=0.8)
 
 #Plot for top right
-plot(elect1$datetm,elect1$Voltage,type="l",ylab="Voltage",xlab="datetime")
+plot(elect1$datetm,elect1$Voltage,type="l",ylab="Voltage",xlab="datetime",cex.axis=0.8,cex.lab=0.8)
 
 #Plot for bottom left
-plot(elect1$datetm,elect1$Sub_metering_1,type="n",col="black",ylim=c(0,37),xlab="",ylab="Energy sub metering")
+plot(elect1$datetm,elect1$Sub_metering_1,type="n",col="black",ylim=c(0,37),xlab="",ylab="Energy sub metering",cex.axis=0.8,cex.lab=0.8)
 lines(elect1$datetm,elect1$Sub_metering_1,type="l",col="black")
 lines(elect1$datetm,elect1$Sub_metering_2,type="l",col="red")
 lines(elect1$datetm,elect1$Sub_metering_3,type="l",col="blue")
@@ -36,6 +36,6 @@ legend( "topright",,
         lty=c(1,1,1),col=c("black","red","blue"),bty = "n",cex=0.8) 
 
 #Plot for bottom right
-plot(elect1$datetm,elect1$Global_reactive_power,type="l",ylim=c(0,0.5),cex.axis=0.8,ylab="Global_reactive_power",xlab="datetime")
+plot(elect1$datetm,elect1$Global_reactive_power,type="l",ylim=c(0,0.5),cex.axis=0.8,cex.lab=0.8,ylab="Global_reactive_power",xlab="datetime")
 
 dev.off()
